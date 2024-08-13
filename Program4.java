f72f119856fd2c9ca8c6ae55ca556f20d0d0592d
mongbean <112592782+mongbean@users.noreply.github.com>
2024-08-13 09:54:22 -0700

    finish program4

--- Program4.java
+++ Program4.java
@@ -1,10 +1,35 @@
 //Youre on your own!
 //Jk ask for help if you need it (but also look at previous programs!!)
-
-
+import java.util.Scanner;
+public class Program4{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a ");
+        int a =  scan.nextInt();
+        System.out.println("b ");
+        int b =  scan.nextInt();
+        System.out.println("c ");
+        int c =  scan.nextInt();
+        System.out.println("d ");
+        int d =  scan.nextInt();
+        int sum = a+b+c+d;
+        double average = (double)sum/4;
+        System.out.println("sum is " + sum + ".");
+        System.out.println("average is "+average+".");
+    }
+}
 
 //Paste console output below:
 /*
-
+a 
+475
+b 
+821
+c 
+369
+d 
+562
+sum is 2227.
+average is 556.75.
 
 */
